f4ac9261a9c8efddcac30bc4da368b241f79918a
abduhaseebmir <mabdulhaseebq@gmail.com>
2023-02-24 16:50:25 +0500

    passing id argument in url

--- inventory_system/urls.py
+++ inventory_system/urls.py
@@ -14,10 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from .views import home_view
+from articles import views
 
 urlpatterns = [
     path("", home_view),
+    path("articles/<int:id>/", views.article_detail_view),
     path('admin/', admin.site.urls),
 ]
