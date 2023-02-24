8ff6655eaf75feef932865cf739110210df34296
abduhaseebmir <mabdulhaseebq@gmail.com>
2023-02-24 16:50:52 +0500

    article detail view

--- articles/views.py
+++ articles/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .models import Article
 
 # Create your views here.
+
+def article_detail_view(request, id=None):
+    article_object = None
+    if id:
+        article_object = Article.objects.get(id=id)
+    context = {
+        "object": article_object
+    }
+    return render(request, "articles/detail.html", context=context)
