41f0c09823f02860bf768c36ced85c02b8b6b87a
abduhaseebmir <mabdulhaseebq@gmail.com>
2023-02-24 16:49:47 +0500

    passing object as context

--- inventory_system/views.py
+++ inventory_system/views.py
@@ -1,15 +1,17 @@
 from articles.models import Article
-from django.http import HttpResponse
+from django.shortcuts import render
 
 def home_view(request):
     article = Article.objects.get(id=1)
+    articles = Article.objects.all()
     context = {
+        "articles": articles,
         "id": article.id,
         "title": article.title,
         "content": article.content
     }
-    html_string = """
-    <h2>{title} (id: {id})</h2>
-    <p>{content}</p>
-    """.format(**context)
-    return HttpResponse(html_string)
+    # html_string = """
+    # <h2>{title} (id: {id})</h2>
+    # <p>{content}</p>
+    # """.format(**context)
+    return render(request, "home-view.html", context)
