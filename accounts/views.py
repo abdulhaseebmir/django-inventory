4dffb7691d3bf19b5f0015b5be32f109953d4fb4
abduhaseebmir <mabdulhaseebq@gmail.com>
2023-02-27 00:33:38 +0500

    user authentication and logout view

--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.contrib.auth import login, logout, authenticate
+from django.shortcuts import render, redirect
 
 # Create your views here.
 
@@ -7,4 +8,18 @@ def login_view(request):
         username = request.POST.get("username")
         password = request.POST.get("password")
         print(f"Username: {username} - Password: {password}")
+        user = authenticate(request, username=username, password=password)
+        if not user:
+            context = {
+                "error": "Invalid username or password"
+            }
+            return render(request, "accounts/login.html", context)
+        login(request, user)
+        return redirect("/")
     return render(request, "accounts/login.html", {})
+
+def logout_view(request):
+    if request.method == "POST":
+        logout(request)
+        return redirect("/login/")
+    return render(request, "accounts/logout.html", {})
