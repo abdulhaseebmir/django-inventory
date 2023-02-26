ae66cbbeb70eeb09e4ffb7a14ce6dcbd9826e249
abduhaseebmir <mabdulhaseebq@gmail.com>
2023-02-27 00:32:59 +0500

    check for user authentication

--- inventory_system/urls.py
+++ inventory_system/urls.py
@@ -17,7 +17,10 @@ from django.contrib import admin
 from django.urls import path, include
 from .views import home_view
 
-from accounts.views import login_view
+from accounts.views import (
+    login_view,
+    logout_view
+)
 
 from articles.views import (
     article_search_view,
@@ -28,6 +31,7 @@ from articles.views import (
 urlpatterns = [
     path("", home_view),
     path("login/", login_view),
+    path("logout/", logout_view),
     path("articles/", article_search_view),
     path("articles/create/", article_create_view),
     path("articles/<int:id>/", article_detail_view),
