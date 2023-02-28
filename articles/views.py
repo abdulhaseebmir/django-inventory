87d25a0de55f13fb3f44d7134511e96665db5dc9
abduhaseebmir <mabdulhaseebq@gmail.com>
2023-02-28 12:48:21 +0500

    Create artcle view refactoring

--- articles/views.py
+++ articles/views.py
@@ -1,5 +1,7 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
+
+from .forms import ArticleForm
 from .models import Article
 
 # Create your views here.
@@ -20,12 +22,32 @@ def article_search_view(request):
 
 @login_required
 def article_create_view(request):
-    if request.method == "POST":
-        title = request.POST.get("title") # see also POST["title"]
-        content = request.POST.get("content") 
-        Article.objects.create(title=title, content=content)
-    context = {}
+    form = ArticleForm(request.POST or None)
+    context = {
+        "form": form
+    }
+    if form.is_valid():
+        article_object = form.save()
+        context["form"] = ArticleForm()
+        # context["object"] = article_object
+        # context["created"] = True
     return render(request, "articles/create.html", context=context)
+# def article_create_view(request):
+#     form = ArticleForm()
+#     context = {
+#         "form": form
+#     }
+#     if request.method == "POST":
+#         form = ArticleForm(request.POST)
+#         context["form"] = form
+#         if form.is_valid():
+#             title = form.cleaned_data.get("title")
+#             content = form.cleaned_data.get("content")
+#             print(title, content)
+#             article_object = Article.objects.create(title=title, content=content)
+#             context["object"] = article_object
+#             context["created"] = True
+#     return render(request, "articles/create.html", context=context)
 
 def article_detail_view(request, id=None):
     article_object = None
